569f9885f4fda9f795c312eee1e85f33a3929a5d
brigitte.nikoll <roadclosed24@gmail.com>
2022-03-20 22:37:59 -0500

    Protegiendo la conexión a la base de datos usando dotenv / variables de entorno

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
+import "dotenv/config";
 
 import { productCtrl } from "./api/controllers/index.js";
 import { productRouter, userRouter } from "./api/routes/index.js";
 const { getAllProducts, getOneProduct, createProduct } = productCtrl;
 
-
 /* MONGOSSE */
 
 //Connect to db
-await mongoose.connect(
-  "mongodb+srv://brigitte:root@clase-02-02.u6sw6.mongodb.net/product_db"
-);
+const dbConnection = process.env.DB_STRING_CONNECTION;
+await mongoose.connect(dbConnection);
 
 //Listener to connection error
 mongoose.connection.on("error", function (e) {
