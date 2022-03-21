670040250806e3537153fd74a0b0609c5e4cc49f
brigitte.nikoll <roadclosed24@gmail.com>
2022-03-21 01:23:22 -0500

    Se mockeo una base de datos, y en postman la petición me devuelve un token

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,16 +1,30 @@
 /* import { User } from "../models/index.js"; */
+import jwt from "jsonwebtoken";
 
+const username = "Bri";
+const password = "1236";
 
-const username = "Bri"
-const password = "1236"
+export const login = async (req, res) => {
+  const { username: userLog, password: passLog } = req.body;
 
-export const login = async (req, res) => { 
+  //Mockeando DB
+  const userDb = {
+    id: "5465d4564-561",
+    name: "Brigitte",
+  };
 
-    const { username: userLog, password: passLog } = req.body;
-
-    if (username === userLog && password === passLog) {
-        res.status(200).json({token: true})
-    } else {
+  if (username === userLog && password === passLog) {
+    //JWT
+    jwt.sign(userDb, process.env.SECRET_KEY, (error, token) => {
+      if (!error) {
+        res.status(200).json({
+          token,
+        });
+      } else {
         res.status(403).send();
-    }
-}
+      }
+    });
+  } else {
+    res.status(403).send();
+  }
+};
